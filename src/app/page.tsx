fcaef91e6b3edf5279ae7ddb9afa8e5f3f4cadca
bodasooqa <timlol9894@gmail.com>
2025-02-08 04:18:22 +0700

    start using theme-toggle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image';
 
+import { ThemeToggle } from '@/components/theme-toggle';
+
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -47,6 +49,7 @@ export default function Home() {
           >
             Read our docs
           </a>
+          <ThemeToggle />
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
