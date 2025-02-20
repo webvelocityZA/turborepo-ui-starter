3be5bd147f98eb5cc0a6c9673d8b48e0b32a7e24
bodasooqa <timlol9894@gmail.com>
2025-02-20 14:54:24 +0700

    integrate tailwind v4 and shadcn/ui

--- apps/trader/src/App.tsx
+++ apps/trader/src/App.tsx
@@ -1,14 +1,16 @@
 import { useState } from "react";
 
+import { Button } from "@/components/ui/button";
+
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <button onClick={() => setCount((count) => count + 1)} type="button">
+    <div className="text-center">
+      <Button onClick={() => setCount((count) => count + 1)} type="button">
         count is {count}
-      </button>
-    </>
+      </Button>
+    </div>
   );
 }
 
--- apps/trader/vite.config.ts
+++ apps/trader/vite.config.ts
@@ -1,7 +1,15 @@
-import react from "@vitejs/plugin-react-swc";
+import path from "node:path";
 import { defineConfig } from "vite";
+
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
