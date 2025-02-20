6d7e7019c26df4eafd036c7985d66506214dee8c
bodasooqa <timlol9894@gmail.com>
2025-02-20 21:23:14 +0700

    move ui components to the package; replace @repo with @workspace

--- apps/trader/src/App.tsx
+++ apps/trader/src/App.tsx
@@ -1,12 +1,13 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
-import { Button } from "@/components/ui/button";
+import { Button } from "@workspace/ui/components/button";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="text-center">
+    <div className="flex flex-col gap-5 items-center justify-center h-screen">
+      <h1 className="text-4xl font-medium">Turborepo + Vite + React + TS + Shadcn/ui + Tailwind CSS v4</h1>
       <Button onClick={() => setCount((count) => count + 1)} type="button">
         count is {count}
       </Button>
