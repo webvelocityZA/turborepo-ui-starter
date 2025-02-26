0cd679729fb97ccc4be6faf8de64c6dad84623d1
bodasooqa <timlol9894@gmail.com>
2025-02-26 23:42:37 +0700

    add NOTION_BLOCK_ID to config validation schema

--- apps/backend/src/config.ts
+++ apps/backend/src/config.ts
@@ -2,6 +2,11 @@ import { z } from "zod";
 
 const configValidationSchema = z.object({
   NOTION_TOKEN: z.string(),
+  /**
+   * https://developers.notion.com/docs/working-with-page-content#creating-a-page-with-content
+   * See the "Where can I find my page's ID?" block about the page ID format
+   */
+  NOTION_BLOCK_ID: z.string().min(32).max(36),
 });
 
 export const validate = (config: Record<string, unknown>) => {
