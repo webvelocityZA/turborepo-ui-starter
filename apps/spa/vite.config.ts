5e5f47eee56a8b4ef2317d3f0204818331d88e2c
bodasooqa <timlol9894@gmail.com>
2025-02-27 03:11:14 +0700

    load environment variables for server port configuration in Vite

--- apps/spa/vite.config.ts
+++ apps/spa/vite.config.ts
@@ -1,16 +1,23 @@
 import path from "node:path";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
 import tailwindcss from "@tailwindcss/vite";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [TanStackRouterVite({ autoCodeSplitting: true }), react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const { PORT } = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [TanStackRouterVite({ autoCodeSplitting: true }), react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+    server: {
+      port: Number.parseInt(PORT),
+    },
+  };
 });
