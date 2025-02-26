dca477550ea98e90884a5213ab03f33393762f1d
bodasooqa <timlol9894@gmail.com>
2025-02-26 23:40:18 +0700

    remove unused ConfigService and related code from AppService

--- apps/backend/src/app.service.ts
+++ apps/backend/src/app.service.ts
@@ -1,14 +1,8 @@
 import { Injectable } from "@nestjs/common";
-import { ConfigService } from "@nestjs/config";
-
-import { Config } from "./config";
 
 @Injectable()
 export class AppService {
-  constructor(private configService: ConfigService<Config, true>) {}
   getHello(): string {
-    const notionToken = this.configService.get("NOTION_TOKEN", { infer: true });
-
     return "Hello World!";
   }
 }
