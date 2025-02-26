10dadbcaea5fd501164abbfd5b8a33aa61c23c89
bodasooqa <timlol9894@gmail.com>
2025-02-26 21:23:30 +0700

    integrate @nestjs/config for environment variable management

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { validate } from "./config";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validate,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/backend/src/app.service.ts
+++ apps/backend/src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+
+import { Config } from "./config";
 
 @Injectable()
 export class AppService {
+  constructor(private configService: ConfigService<Config, true>) {}
   getHello(): string {
+    const notionToken = this.configService.get("NOTION_TOKEN", { infer: true });
+
     return "Hello World!";
   }
 }
