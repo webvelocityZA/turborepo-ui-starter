c5b3fb324cfe55f347a888d3d2b140812e006d3d
bodasooqa <timlol9894@gmail.com>
2025-03-02 02:01:31 +0700

    update API base URL to include '/api' path

--- apps/spa/src/services/APINetworkService/index.ts
+++ apps/spa/src/services/APINetworkService/index.ts
@@ -3,5 +3,5 @@ import { CONFIG } from "@/config";
 import { APINetworkService as APINetworkServiceClass } from "./APINetworkService.ts";
 
 export const APINetworkService = new APINetworkServiceClass({
-  baseURL: CONFIG.VITE_API_BASE_URL,
+  baseURL: `${CONFIG.VITE_API_BASE_URL}/api`,
 });
