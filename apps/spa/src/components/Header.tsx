72f110feee1ce748131f7ef3e63f5528fde081d8
bodasooqa <timlol9894@gmail.com>
2025-03-02 02:36:29 +0700

    style(Header): update header component to be fixed with backdrop blur and shadow

--- apps/spa/src/components/Header.tsx
+++ apps/spa/src/components/Header.tsx
@@ -4,7 +4,16 @@ import { cn } from "@workspace/ui/lib/utils";
 
 export const Header = React.forwardRef<HTMLDivElement, React.ComponentPropsWithRef<"div">>(
   ({ children, className, ...props }, ref) => (
-    <div {...props} ref={ref} className={cn("container flex items-center justify-between py-2", className)}>
+    <div
+      {...props}
+      ref={ref}
+      className={cn(
+        "fixed left-0 top-0",
+        "container flex items-center justify-between py-2",
+        "backdrop-blur-sm border-b shadow",
+        className,
+      )}
+    >
       Logo
       {children}
     </div>
