599d1002aebdf6cd535f788432869df61186f506
bodasooqa <timlol9894@gmail.com>
2025-03-02 22:42:43 +0700

    refactor root routing components to include loading and error states with dedicated components

--- apps/spa/src/routes/__root.tsx
+++ apps/spa/src/routes/__root.tsx
@@ -1,5 +1,6 @@
 import type { QueryClient } from "@tanstack/react-query";
-import { Outlet, createRootRouteWithContext } from "@tanstack/react-router";
+import { type ErrorComponentProps, Outlet, createRootRouteWithContext } from "@tanstack/react-router";
+import { LoaderCircle, TriangleAlert } from "lucide-react";
 import React, { Suspense } from "react";
 
 import type { useTonWallet } from "@workspace/ton-connect-sdk-react-ui";
@@ -21,7 +22,13 @@ type RouterContext = {
 };
 
 export const Route = createRootRouteWithContext<RouterContext>()({
-  component: () => (
+  component: RootRouteComponent,
+  pendingComponent: RootRoutePendingComponent,
+  errorComponent: RootRouteErrorComponent,
+});
+
+function RootRouteComponent() {
+  return (
     <>
       <HeaderContainer />
       <main className="container mx-auto pt-4 mt-[var(--header-height)]">
@@ -31,5 +38,30 @@ export const Route = createRootRouteWithContext<RouterContext>()({
         <TanStackRouterDevtools />
       </Suspense>
     </>
-  ),
-});
+  );
+}
+
+function RootRoutePendingComponent() {
+  return (
+    <RootRouteWrapper>
+      <LoaderCircle size={80} className="text-primary animate-spin" />
+      <h1 className="text-2xl font-bold">Loading</h1>
+    </RootRouteWrapper>
+  );
+}
+
+function RootRouteErrorComponent({ error }: ErrorComponentProps) {
+  return (
+    <RootRouteWrapper>
+      <TriangleAlert size={80} className="text-destructive" />
+      <h1 className="text-2xl font-bold">Error</h1>
+      <p className="text-lg text-gray-500">
+        {error.message ? error.message : "An error occurred while rendering this route."}
+      </p>
+    </RootRouteWrapper>
+  );
+}
+
+function RootRouteWrapper({ children }: { children: React.ReactNode }) {
+  return <div className="flex flex-col justify-center items-center w-full h-screen">{children}</div>;
+}
