6a0608208a5db1f2fda4f926726cff9e151f2eb3
bodasooqa <timlol9894@gmail.com>
2025-03-03 02:49:29 +0700

    add logo image and update URLs in manifest and header for improved branding

--- apps/spa/src/components/Header.tsx
+++ apps/spa/src/components/Header.tsx
@@ -2,6 +2,8 @@ import React from "react";
 
 import { cn } from "@workspace/ui/lib/utils";
 
+import { CONFIG } from "@/config";
+
 export const Header = React.forwardRef<HTMLDivElement, React.ComponentPropsWithRef<"div">>(
   ({ children, className, ...props }, ref) => (
     <div
@@ -10,7 +12,7 @@ export const Header = React.forwardRef<HTMLDivElement, React.ComponentPropsWithR
       className={cn("fixed w-full left-0 top-0", "backdrop-blur-sm bg-background/80 border-b shadow", className)}
     >
       <div className="container flex items-center justify-between py-2">
-        <img src="public/logo.png" alt="logo" className="h-10" />
+        <img src={`${CONFIG.VITE_API_BASE_URL}/files/logo.png`} alt="logo" className="h-10" />
         {children}
       </div>
     </div>
