0ff60faff403b0f80b486bcd6791d5d07f40d415
bodasooqa <timlol9894@gmail.com>
2025-03-03 02:43:07 +0700

    invalidate router on wallet change to ensure correct redirection

--- apps/spa/src/containers/HeaderContainer.tsx
+++ apps/spa/src/containers/HeaderContainer.tsx
@@ -1,11 +1,15 @@
+import { useRouter } from "@tanstack/react-router";
 import { useEffect, useRef } from "react";
 
-import { TonConnectButton } from "@workspace/ton-connect-sdk-react-ui";
+import { TonConnectButton, useTonWallet } from "@workspace/ton-connect-sdk-react-ui";
 
 import { Header } from "@/components/Header";
 
 export const HeaderContainer: React.FC<React.ComponentPropsWithoutRef<typeof Header>> = (props) => {
   const headerRef = useRef<HTMLDivElement>(null);
+
+  const router = useRouter();
+  const wallet = useTonWallet();
 
   useEffect(() => {
     const handleResize = () => {
@@ -20,6 +24,16 @@ export const HeaderContainer: React.FC<React.ComponentPropsWithoutRef<typeof Hea
       window.removeEventListener("resize", handleResize);
     };
   }, []);
+
+  /**
+   * biome-ignore lint/correctness/useExhaustiveDependencies:
+   * We dont need to add router to the dependencies because
+   * we only want to invalidate the router when the wallet changes
+   * It allows us to redirect the user to the correct route
+   */
+  useEffect(() => {
+    router.invalidate();
+  }, [wallet]);
 
   return (
     <Header {...props} ref={headerRef}>
