bba688d8b454ce203fbd7944cc0a6cc54636a678
bodasooqa <timlol9894@gmail.com>
2025-03-02 18:35:15 +0700

    add VITE_TON_API_BASE_URL to configuration

--- apps/spa/src/config.ts
+++ apps/spa/src/config.ts
@@ -3,4 +3,5 @@ import { parseEnv, port, z } from "znv";
 export const CONFIG = parseEnv(import.meta.env, {
   VITE_PORT: port().default(3000),
   VITE_API_BASE_URL: z.string().url(),
+  VITE_TON_API_BASE_URL: z.string().url(),
 });
