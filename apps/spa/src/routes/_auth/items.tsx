d9a0335be5befdc86dfcaee637e5bb422dec3a8a
bodasooqa <timlol9894@gmail.com>
2025-03-02 21:59:02 +0700

    add addresses query options and loader for NFT items in auth route

--- apps/spa/src/routes/_auth/items.tsx
+++ apps/spa/src/routes/_auth/items.tsx
@@ -1,10 +1,57 @@
+import { queryOptions, useSuspenseQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
-import React from "react";
+
+import { Address } from "@workspace/entities";
+
+import { NFTItemCard } from "@/components/NFTItemCard";
+import { NFTItem } from "@/entities/NFTItem";
+import { APINetworkService } from "@/services/APINetworkService";
+import { TonAPINetworkService } from "@/services/TonAPINetworkService";
+
+const addressesQueryOptions = queryOptions({
+  queryKey: ["addresses"],
+  queryFn: async () => {
+    const { data } = await APINetworkService.getAddresses();
+
+    /**
+     * Remove duplicates and create Address instances
+     * to ensure that the addresses are valid
+     */
+    const addresses = [...new Set(data)].flatMap((nftItem) => Address.safeCreate(nftItem) ?? []);
+
+    if (addresses.length === 0) return [];
+
+    const { nft_items } = await TonAPINetworkService.getNFTsByAddresses(addresses.map((nftItem) => nftItem.toString()));
+
+    return nft_items.map(
+      ({ address, metadata, previews }) =>
+        new NFTItem({
+          name: metadata.name,
+          description: metadata.description,
+          image: previews.find((preview) => preview.resolution === "500x500")?.url ?? metadata.image,
+          address: new Address(address),
+        }),
+    );
+  },
+  refetchInterval: 60_000 * 10, // 10 minutes
+  staleTime: 60_000 * 10, // 10 minutes
+});
 
 export const Route = createFileRoute("/_auth/items")({
   component: RouteComponent,
+  loader: ({ context: { queryClient } }) => queryClient.ensureQueryData(addressesQueryOptions),
+  pendingComponent: () => <div>Loading</div>,
+  staleTime: 10_000, // 10 seconds
 });
 
 function RouteComponent() {
-  return <div>Hello "/_auth/items"!</div>;
+  const { data } = useSuspenseQuery(addressesQueryOptions);
+
+  return (
+    <div className="flex flex-col gap-4">
+      {data.map((nftItem) => (
+        <NFTItemCard key={nftItem.address.toString()} nftItem={nftItem} />
+      ))}
+    </div>
+  );
 }
