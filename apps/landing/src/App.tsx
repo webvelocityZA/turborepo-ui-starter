19825f126096912433ca01538a0de331a0e320ef
bodasooqa <timlol9894@gmail.com>
2025-03-03 23:43:03 +0700

    start using TimelineSection

--- apps/landing/src/App.tsx
+++ apps/landing/src/App.tsx
@@ -1,9 +1,11 @@
 import { HeroSection } from "@/components/HeroSection";
+import { TimelineSection } from "@/components/TimelineSection";
 
 export const App: React.FC = () => {
   return (
     <>
       <HeroSection />
+      <TimelineSection />
     </>
   );
 };
