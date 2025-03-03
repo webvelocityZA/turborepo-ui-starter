c7133720b4dde9b04043fa1a26eef43752e6c474
bodasooqa <timlol9894@gmail.com>
2025-03-03 15:57:04 +0700

    fix: update iconUrl paths in tonconnect-manifest.json and Header component

--- apps/spa/src/components/Header.tsx
+++ apps/spa/src/components/Header.tsx
@@ -10,7 +10,7 @@ export const Header = React.forwardRef<HTMLDivElement, React.ComponentPropsWithR
       className={cn("z-[1] fixed w-full left-0 top-0 backdrop-blur-sm bg-background/80 border-b shadow", className)}
     >
       <div className="container flex items-center justify-between py-2">
-        <img src="public/logo.png" alt="logo" className="h-10" />
+        <img src="/logo.png" alt="logo" className="h-10" />
         {children}
       </div>
     </div>
