55179ee1de1a70b81a2f793f241b9f28b99b85a2
bodasooqa <timlol9894@gmail.com>
2025-03-04 16:01:18 +0700

    start using MotivationSection

--- apps/landing/src/App.tsx
+++ apps/landing/src/App.tsx
@@ -1,10 +1,12 @@
 import { HeroSection } from "@/components/HeroSection";
 import { TimelineSection } from "@/components/TimelineSection";
+import { MotivationSection } from "./components/MotivationSection";
 
 export const App: React.FC = () => {
   return (
     <>
       <HeroSection />
+      <MotivationSection />
       <TimelineSection />
     </>
   );
